a96a9c091daff904a20d3bd18dc662c80ea2f42f
Christopher <christopher.drewes@outlook.com>
2022-07-29 20:17:03 +0200

    Start

---
+++ src/main/java/com/example/hypewave/Bill.java
@@ -0,0 +1,13 @@
+package com.example.hypewave;
+
+public class Bill {
+    public String Influencer;
+    public int Betrag;
+    public String Typ;
+
+    public Bill(String influencer, int betrag, String typ) {
+        Influencer = influencer;
+        Betrag = betrag;
+        Typ = typ;
+    }
+}
--- src/main/java/com/example/hypewave/MainController.java
+++ src/main/java/com/example/hypewave/MainController.java
@@ -4,14 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,35 +22,37 @@ public class MainController implements Initializable {
     public AnchorPane calculatePane;
 
     public AnchorPane billPane;
+    public TextField billInputInfluencer;
+    public TextField billInputBetrag;
+    public ComboBox billInputTyp;
+    public Button billSubmit;
 
     private List<Pane> mainPanes = new ArrayList<>();
+
+    private Connection con;
+
+    private List<Bill> bills = new ArrayList<>();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         mainPanes.add(billPane);
         mainPanes.add(calculatePane);
-
-        initSQL();
+        billInputTyp.getItems().addAll("Instagram Story", "TikTok Beitrag", "Instagram Reel");
+        try {
+            initSQL();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void initSQL(){
-        String connectionUrl =
-                "jdbc:sqlserver://Chrissi.database.windows.net:1433;"
-                        + "database=AdventureWorks;"
-                        + "user=Chrissi@Chrissi;"
-                        + "password=Chrissi123;"
-                        + "encrypt=true;"
-                        + "trustServerCertificate=false;"
-                        + "loginTimeout=30;";
+    public void initSQL() throws SQLException {
+        String connectionUrl = "jdbc:mysql://localhost:3306/Hypewave";
+        String user = "root";
+        String password = "Chrissi123";
+        con = DriverManager.getConnection(connectionUrl, user, password);
 
-        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
-            System.out.println("connected");
-        }
-        // Handle any errors that may have occurred.
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
+
     }
 
 
@@ -64,12 +67,26 @@ public class MainController implements Initializable {
         }
     }
 
-    public void getBillInfos(){
+    public void getBillInfos() throws SQLException {
+        String query  ="SELECT * FROM Bills";
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
+
+        bills = new ArrayList<>();
+        while (rs.next()){
+            bills.add(new Bill(rs.getString(1), Integer.parseInt(rs.getString(2)), rs.getString(3)));
+        }
 
     }
 
-    public void saveBillToSQL(){
-        jdbc:mysql://127.0.0.1:3306/?user=Chrissi
+    public void saveBillToSQL() throws SQLException {
+        String influencer = billInputInfluencer.getText();
+        int betrag = Integer.parseInt(billInputBetrag.getText());
+        String typ = billInputTyp.getItems().get(0).toString();
+        String query = "INSERT INTO Bills (Influencer, Betrag, Typ) VALUES ('" + influencer + "', " + betrag + ", ' " + typ + "');";
+        Statement stmt = con.createStatement();
+        stmt.execute(query);
+        stmt.close();
     }
 
 
